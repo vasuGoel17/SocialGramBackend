d5c72a1529a09673130ca8d96d891c0c037f82df
VasuGoel17 <goelvasu17@gmail.com>
2024-02-01 22:25:15 +0530

    fourth commit

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,43 @@
+// const mongoose = require("mongoose");
+// const DB = process.env.DB;
+// mongoose
+//   .connect(DB, {})
+//   .then(() => console.log("connection start"))
+//   .catch((err) => {
+//     console.log("error in connecting to mongoDB as: ", err.message);
+//   });
+
 const mongoose = require("mongoose");
 const DB = process.env.DB;
+
 mongoose
-  .connect(DB, {})
-  .then(() => console.log("connection start"))
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
   .catch((err) => {
-    console.log(err.message);
-    
+    console.error("Error connecting to MongoDB:", err);
   });
 
+// Event listeners for MongoDB connection events
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to DB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Mongoose connection error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
+});
+
+// Graceful shutdown on application termination
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log(
+      "Mongoose connection terminated due to application termination"
+    );
+    process.exit(0);
+  });
+});
